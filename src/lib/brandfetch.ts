2964759bb96d9b7228e3de12613eb1ccdbcff687
Kris McNair <kmcnair_100@outlook.com>
2025-05-22 14:05:49 -0700

    update API key

--- src/lib/brandfetch.ts
+++ src/lib/brandfetch.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { getBusinessDomain } from './utils/domain'
 
 const BRANDFETCH_API_KEY = '1id07tPwKlmKE0Dbd-w'
-const LOGODEV_API_KEY = 'sk_JkxtsBv9QDCSqWVyxiRyrw'
+const LOGODEV_API_KEY = 'pk_IUW5ADLFTyaB0B8WF5lwtA'
 
 const logoResponseSchema = z.array(z.object({
   url: z.string().url(),
